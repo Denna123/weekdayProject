4741b200255ab479f4bfa43b0b0b8df471e05687
Denna123 <denna5454@gmail.com>
2022-04-26 20:19:34 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//jshint sversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use("view engine", "ejs");
+app.get("/", function(req, res) {
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.write("<h1>Yay, it's the weekend!!!</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+    }
+});
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000")
+});
