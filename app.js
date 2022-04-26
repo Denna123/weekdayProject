7731bd0fd77938b355150740c725d7ebeaaddb0f
Denna123 <denna5454@gmail.com>
2022-04-26 21:01:27 +0800

    ejs activate

--- app.js
+++ app.js
@@ -5,17 +5,59 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Yay, it's the weekend!!!</h1>");
-    } else {
-        res.sendFile(__dirname + "/index.html");
+    switch (currentDay) {
+      case 0:
+        day = "Sunday";
+        break;
+      case 1:
+        day = "Monday";
+        break;
+
+      case 2:
+        day = "Tuesday";
+        break;
+      case 3:
+        day = "Wednesday";
+        break;
+      case 4:
+        day = "Thursday";
+        break;
+      case 5:
+        day = "Friday";
+        break;
+      case 6:
+        day = "Saturday";
+        break;
+
+      default:
+        break;
+        console.log("Error: current day is equal to: " + currentDay)
     }
+
+
+
+    // if (currentDay === 6 || currentDay === 0) {
+    //     day ="Weekend";
+    // }else if (currentDay === 1 ){
+    //     day ="Monday";
+    // } else if (currentDay === 2 ){
+    //     day ="Tuesday";
+    // } else if (currentDay === 3 ){
+    //     day ="Wednesday";
+    // } else if (currentDay === 4){
+    //     day ="Thursday";
+    // }  else {
+    //     day = "Friday";
+    // }
+
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function() {
