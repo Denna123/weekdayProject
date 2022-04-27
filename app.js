912d10c2418f962c0f35eae32b5800d3fb1f7fd8
Denna123 <denna5454@gmail.com>
2022-04-27 19:23:53 +0800

    Crete "work" route

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -73,16 +74,29 @@ app.get("/", function(req, res) {
     //     day = "Friday";
     // }
  
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
+
+
+  
   var item = req.body.newItem;
 
   items.push(item);
 
   res.redirect("/");
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+  let item =req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000")
